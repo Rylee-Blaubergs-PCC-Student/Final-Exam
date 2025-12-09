33eebe247eafd3b1a62823d2b3ce7ccd6e2214ec
Rylee-Blaubergs-PCC-Student <rylee.blaubergs@pcc.edu>
2025-12-09 12:56:49 -0800

    Create FinalExam.cpp

---
+++ FinalExam.cpp
@@ -0,0 +1,74 @@
+// ------------- FILE HEADER -------------
+// Author ✅: Rylee Blaubergs
+// Assignment ✅: 
+// Date ✅: 
+// Citations: zyBooks
+
+
+// ------------- ZYBOOKS SCORES -------------
+// Chapter ✅: 
+// Participation ✅:
+// Challenge ✅: 
+// Labs ✅: 
+
+
+// ------------- DISCORD POSTS -------------
+// https://discord.com/invite/URYKKf8YHm
+// Count ✅: 0
+// Links (Optional): 
+
+
+// ------------- DESIGN DOCUMENT -------------
+// A. INPUT ✅: 
+// B. OUTPUT ✅: 
+// C. CALCULATIONS ✅: 
+// D. LOGIC and ALGORITHMS ✅:
+
+
+
+// ------------- TESTING -------------
+// PASS ALL GIVEN SAMPLE RUN TESTS ✅: 
+// (Optional) Additional tests count:   
+
+
+// ------------- CODE -------------
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+// Main function
+// https://en.cppreference.com/w/cpp/language/main_function.html
+int main(int argc, char* argv[]) {
+
+   return 0;
+}
+
+// Function implementations (if any)
+
+
+// ------------- DESIGN -------------
+/* 
+Program Name: 
+
+Program Description: 
+
+Design:
+A. INPUT
+
+
+B. OUTPUT
+
+
+C. CALCULATIONS
+
+
+D. LOGIC and ALGORITHMS
+
+
+SAMPLE RUNS:
+
+
+
+
+
+*/
