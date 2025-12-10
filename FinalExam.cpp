5ba930020fd57247d58e04c6bb9d0f37cec5755b
Rylee-Blaubergs-PCC-Student <rylee.blaubergs@pcc.edu>
2025-12-10 00:33:23 +0000

    update

--- FinalExam.cpp
+++ FinalExam.cpp
@@ -1,8 +1,8 @@
 // ------------- FILE HEADER -------------
 // Author ✅: Rylee Blaubergs
-// Assignment ✅: 
-// Date ✅: 
-// Citations: zyBooks
+// Assignment ✅: Final Exam
+// Date ✅: 12/9/2025
+// Citations: zyBooks and previous sample programs
 
 
 // ------------- ZYBOOKS SCORES -------------
@@ -39,8 +39,66 @@ using namespace std;
 // Main function
 // https://en.cppreference.com/w/cpp/language/main_function.html
 int main(int argc, char* argv[]) {
+// Declare input values
+int goal = 0, miles = 0;
+
+// Declare output values
+int totalMiles = 0, milesOverUnder = 0;
+
+// hidden values
+int count = 0;
+
+
+welcome();
+
+goal = readGoal();
+
+
+
+
+goodbye();
 
    return 0;
+}
+
+void welcome() {
+cout << "Welcome to my Miles Tracker program." << endl;
+}
+
+void results() {
+
+
+}
+
+void goodBye() {
+cout << "Keep Riding" << endl;
+}
+
+int readGoal(int goal) {
+// Prompt user for goal
+cout << "How many miles do you want to ride this week?";
+// Read goal input
+int goal = readInt();
+
+if (goal < 0) {
+  cout << "No miles were tracked this week." << endl;
+}
+
+return goal;
+}
+
+int readInt() {
+  int tempVar = 0;
+  cin >> tempVar;
+  while(!cin)
+  {
+    cout << "Invalid input! Please try again!!" << endl;
+    cin.clear();
+    cin.ignore(100, '\n');
+    cin >> tempVar;
+  }
+  cin.ignore(10, '\n');
+  return tempVar;
 }
 
 // Function implementations (if any)
